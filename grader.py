d487e94af1ed3ec2efa007ff4ae98ac8ff642933
Defy <61808223+De-y@users.noreply.github.com>
2024-01-11 20:04:24 -0800

    Update grader.py

--- grader.py
+++ grader.py
@@ -1,4 +1,4 @@
-def compare_answers(answers_file, responses_file):
+def grade(answers_file, responses_file):
     with open(answers_file, 'r') as e_file, open(responses_file, 'r') as a_file:
         correct_answers = e_file.read().splitlines()
         user_responses = a_file.read().splitlines()
@@ -8,4 +8,4 @@ def compare_answers(answers_file, responses_file):
                 wrong_amount = wrong_amount + 1
                 print(f"Question {i}: Your answer was {response}, correct answer is {correct}")
         print(f"\n\n[ANALYSIS]\n\nYou got {wrong_amount} questions wrong, and {54 - wrong_amount} right.\nThat is a about\n{round(((54-wrong_amount)/54) * 100,2)}% correct, and {round((wrong_amount/54)*100,2)}% wrong.\nDo better next time!\n")
-compare_answers('answers.txt', 'responses.txt')
+grade('answers.txt', 'responses.txt')
